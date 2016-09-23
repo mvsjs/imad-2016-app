e303a7921dda7b8db6dde998ce30b1c5bcb5c20b
mvsjs <seetharaman.mv@gmail.com>
2016-09-23 21:55:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,6 +99,12 @@ app.get('/counter', function (req, res) {
   res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res) {
+    var name = req.query.name;
+    names.push(name);
+});
+
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
     //articles[articleName] == {} content object for article one
@@ -126,13 +132,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name/:name', function (req, res) {
-    var name = req.params.name;
-    names.push(name);
-    
-    res.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
