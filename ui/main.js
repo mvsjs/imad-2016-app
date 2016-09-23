a7cf6fb6eee9e8f4f06d026fe712f7156fd25d76
mvsjs <seetharaman.mv@gmail.com>
2016-09-23 22:20:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,19 +9,25 @@ var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i=0; i < names.length; i++) {
+                list += '<li>' + names[i] + '</li>';
+}
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
             }
         }
     };
-    request.open('GET','http://mvsjs.imad.hasura-app.io/counter', true);
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
+    request.open('GET','http://mvsjs.imad.hasura-app.io/submit-name' + name, true);
     request.send(null);
     
 };
 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
     
