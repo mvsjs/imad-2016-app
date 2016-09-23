6e875f64fe19d8ec3b838b2e793882c8d1a5a88c
mvsjs <seetharaman.mv@gmail.com>
2016-09-23 22:04:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -103,6 +103,7 @@ var names = [];
 app.get('/submit-name', function (req, res) {
     var name = req.query.name;
     names.push(name);
+    res.send(JSON.stringify(names));
 });
 
 app.get('/:articleName', function (req, res) {
